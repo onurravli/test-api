491a4cdef700be47864d00ae48149f1890d4f4a1
onurravli <onurravli@hotmail.com>
2023-01-06 13:06:31 +0300

    Update files

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const app = require('express')();
+const express = require('express')();
 const PORT = 8080;
-
 
 const { v4 } = require('uuid');
 
-app.get('/api', (req, res) => {
+express.get('/api', (req, res) => {
     const path = `/api/item/${v4()}`;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+express.get('/api/item/:slug', (req, res) => {
     const { slug } = req.params;
     res.end(`Item: ${slug}`);
 });
@@ -24,13 +23,13 @@ const tokens = [
     "ravlico"
 ]
 
-app.get('/random:start:end', (req, res) => {
+express.get('/random:start:end', (req, res) => {
     res.status(200).send({
         random: Math.floor(Math.random() * 10)
     })
 })
 
-app.get('/kahve', (req, res) => {
+express.get('/kahve', (req, res) => {
     if (tokens.includes(req.get('cookie'))) {
         res.status(200).send({
             kahve: 'guzeldir',
@@ -45,7 +44,7 @@ app.get('/kahve', (req, res) => {
     }
 });
 
-app.listen(
+express.listen(
     PORT,
     () => {
         console.log("Server is running on port: " + PORT);
