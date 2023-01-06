8bb1c84b57ca2ab34a185ee605dc9c9cf7fe5317
onurravli <onurravli@hotmail.com>
2023-01-06 13:02:28 +0300

    Update api

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const app = require('express')();
 const PORT = 8080;
 
+
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+});
+
+
+
 const tokens = [
     "memoli",
     "onur",
     "ravlico"
 ]
 
-app.get('/random', (req, res) => {
+app.get('/random:start:end', (req, res) => {
     res.status(200).send({
         random: Math.floor(Math.random() * 10)
     })
